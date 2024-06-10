b581620190152ecfc79ab6d93959ae6aa3e1d358
Muhammad-Ullah <76475254+Muhammad-Ullah@users.noreply.github.com>
2024-06-10 17:16:59 +0500

    First Commit

---
+++ lib/Views/AuthScreen/WelcomeScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class welcomeScreen extends StatefulWidget {
+  const welcomeScreen({super.key});
+
+  @override
+  State<welcomeScreen> createState() => _welcomeScreenState();
+}
+
+class _welcomeScreenState extends State<welcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
